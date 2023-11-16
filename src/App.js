8a37f9b648bc0b2d6acfc4608f24136a8d1514b0
Shreesha Aatreya <shreeshaaatreya1@gmail.com>
2023-11-16 20:55:53 +0545

    Updated

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div class="nav-bar">
+      <p id="signup">Signup</p>
+      <p id="login">Login</p>
+      <p id="home">Home</p>
+      <p id="about">About</p>
+      <p id="contact">Contact</p>
+    </div>
+  );
+}
+
+export default App;
