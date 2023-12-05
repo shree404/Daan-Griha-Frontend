aff28eeb2df253b5debf5347332600703919e3bc
Shreesha Aatreya <shreeshaaatreya1@gmail.com>
2023-12-05 15:38:25 +0545

    Updated

--- src/components/nav.js
+++ src/components/nav.js
@@ -2,53 +2,31 @@ import React, { useState } from "react";
 import "./nav.css";
 
 function Nav() {
-  const [ishovered, sethover] = useState(false);
-  const mouseHandle = () => {
-    sethover(true);
-  };
-  const mouseLeave = () => {
-    sethover(false);
-  };
   return (
     <div>
       <nav className="nav-bar">
         <p
           id="signup"
-          // style={style}
-          onMouseEnter={mouseHandle}
-          onMouseLeave={mouseLeave}
         >
           Signup
         </p>
         <p
           id="login"
-          // style={style}
-          onMouseEnter={mouseHandle}
-          onMouseLeave={mouseLeave}
         >
           Login
         </p>
         <p
           id="home"
-          // style={style}
-          onMouseEnter={mouseHandle}
-          onMouseLeave={mouseLeave}
         >
           Home
         </p>
         <p
           id="about"
-          // style={style}
-          onMouseEnter={mouseHandle}
-          onMouseLeave={mouseLeave}
         >
           About
         </p>
         <p
           id="contact"
-          // style={style}
-          onMouseEnter={mouseHandle}
-          onMouseLeave={mouseLeave}
         >
           Contact
         </p>
